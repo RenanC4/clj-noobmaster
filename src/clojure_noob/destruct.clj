b00fd2c231a8ac348c65120fdcb81f937d5b977b
Renan Campos <renanc433@gmail.com>
2021-07-13 19:09:40 -0300

    filtros

---
+++ src/clojure_noob/destruct.clj
@@ -0,0 +1,27 @@
+(ns clojure-noob.destruct)
+
+(defn preco-total-do-produto [produto]
+  (* (:quantidade produto) (:preco produto)))
+
+(def pedido {:mochila {:quantidade 1 :preco 5}
+             :cadeira {:quantidade 2 :preco 12}})
+
+(defn total-pedido
+  [pedido]
+  (->> pedido
+       vals
+       (map preco-total-do-produto)
+       (reduce +)))
+
+(total-pedido pedido)
+
+
+(def pedido {:mochila {:quantidade 1 :preco 5}
+             :cadeira {:quantidade 2 :preco 12}
+             :chaveiro {:quantidade 2}})
+
+(defn gratuito?
+  [[chave item]]
+  (<= (get item :preco 0) 0))
+
+(filter gratuito? pedido)
--- src/clojure_noob/maps_values_keys.clj
+++ src/clojure_noob/maps_values_keys.clj
@@ -21,3 +21,5 @@
     :cadeira
     :preco
     println)
+
+
