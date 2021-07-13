537fc620c705f3a96ca94d50f4c6bd92811c53fd
Renan Campos <renanc433@gmail.com>
2021-07-13 16:37:11 -0300

    maps values and keys

---
+++ src/clojure_noob/maps_values_keys.clj
@@ -0,0 +1,23 @@
+(ns clojure-noob.maps-values-keys)
+
+(def estoque {:mochila 10
+              :camiseta 5})
+
+(println estoque)
+(count estoque)
+(keys estoque)
+(vals estoque)
+(assoc estoque :mochila 12)
+(dissoc estoque :mochila)
+(update estoque :mochila inc)
+(update estoque :mochila dec)
+
+(def pedido {:mochila {:quantidade 1 :preco 5}
+             :cadeira {:quantidade 2 :preco 12}})
+
+(assoc pedido :camiseta {:preco 25 :quantidade 2})
+
+(-> pedido
+    :cadeira
+    :preco
+    println)
